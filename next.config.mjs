d1665b563b745e6d02fc22214aa0394e50b6d5f0
Theodore Mentis <th.mentis@gmail.com>
2024-09-14 20:23:50 +0300

    added Content-Security-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -33,6 +33,11 @@ const nextConfig = {
             key: "Permissions-Policy",
             value: "camera=(), microphone=(), geolocation=()",
           },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline' https:; img-src 'self' data: https:; font-src 'self' data: https:; connect-src 'self' https:; frame-src 'self' https:;",
+          },
         ],
       },
     ];
